8a1bbcab450dabcc9a3f14c88ddae1ded501a6c4
SubuserJava <telemarent@yandex.ru>
2022-08-14 19:53:44 +0300

    Перенос из Practice в HomeWork домашних задач по Collections. Practice23, Practice24

---
+++ src/com/company/homework/HomeWork_Practice24/Task1/Boat.java
@@ -0,0 +1,9 @@
+package com.company.homework.HomeWork_Practice24.Task1;
+
+public class Boat {
+
+    private boolean finalIntermediateStopOfBoat;
+    private int capacityOfBoat;
+    private int amountEmptySeatsOfBoat;
+
+}
---
+++ src/com/company/homework/HomeWork_Practice24/Task1/Pier.java
@@ -0,0 +1,9 @@
+package com.company.homework.HomeWork_Practice24.Task1;
+
+import java.time.LocalTime;
+
+public class Pier {
+
+    public int averageTimeBetweenPassengers;
+    public LocalTime averageTimeBetweenBoats;
+}
